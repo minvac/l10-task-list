abc87bfe971b15fb2d954a705a1a789b48608b3a
bmenc <bmenchaca.cca@gmail.com>
2024-06-15 14:10:19 -0600

    chore seed on db

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,15 +3,13 @@
 @section('title', 'The list of tasks')
 
 @section('content')
-   <div>
-      @if (count($tasks))
-         @forelse ($tasks as $task)
-         <div>
-               <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-         </div>
-         @empty
-               <div>There are no tasks</div>
-         @endforelse
-      @endif
-   </div>
+   @if (count($tasks))
+      @forelse ($tasks as $task)
+      <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+      </div>
+      @empty
+            <div>There are no tasks</div>
+      @endforelse
+   @endif
 @endsection('content')
